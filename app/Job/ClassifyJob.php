6a1169da313606d7346eb68bad3d92718edfde1f
root <421528826@qq.com>
2021-02-28 00:56:14 +0800

    优化代码

--- app/Job/ClassifyJob.php
+++ app/Job/ClassifyJob.php
@@ -29,6 +29,7 @@ class ClassifyJob extends Job
                 try {
                     $search_result = $jds->spider(10)->search();
                 } catch (\Throwable $e) {
+                    echo $e->getMessage() . PHP_EOL;
                     continue;
                 }
                 collect($search_result)->each(function ($subject, $key) {
